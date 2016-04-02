204f129954fe2067d6fab2a73aa0bf1dcd5e0035
Chris Paradis <paradc2@rpi.edu>
2016-04-02 15:25:57 -0400

    addition of theft and taxi r code

--- BikepathData.R
+++ BikepathData.R
@@ -8,3 +8,20 @@ y = count(bike_routes, 'TOSTREET')
 x = count(bike_routes, 'FROMSTREET')
 write.csv(y, file = "bikeroutecount.csv")
 write.csv(x, file = "bikeroutecount2.csv")
+
+
+#taxi data
+#library("ff")
+#x<- read.csv.ffdf(file="~/Downloads/trip_data_1.csv", header=TRUE, VERBOSE=TRUE, first.rows=10000, next.rows=50000, colClasses=NA)
+
+taxi_df <- read.csv(file="~/Downloads/trip_data_1.csv",nrows=1000000)
+View(taxi_df)
+
+theft_df <- read.csv(file="~/Downloads/Thefts2015.csv",nrows=1000000)
+View(theft_df)
+
+#bikes <- c("Bike", "Bicycle")
+selectedRows <- theft_df[grep('(Bike|Bicycle)', theft_df$Complaint.Type), ]
+View(selectedRows)
+#grepl("Blocked Driveway", theft_df$Complaint.Type)
+
